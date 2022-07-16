9ffb25ffd43bd4fb05b41f7cba8cca537fdf8336
saumyaarora80 <60285951+saumyaarora80@users.noreply.github.com>
2022-07-16 10:42:16 +0530

    Add files via upload

---
+++ PYTHON PRACTISE.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#new fratures introduce in java 8
+#java 17 ia N LTS VERSION
+
+
+# 
+# # array
+
+# In[2]:
+
+
+#data structure= arrangement of data in a computers memory or disk. it have array, queues,linked list etc
+#major operation = searching traversing inserting deleting etc
+#advantages of data storage = use in complexx problems, allows safe storage of information etc
+#linear data structure= sequence order arangement
+#we use data structure in compiler os dbms graphics RDBMS etc
+
+
+# In[3]:
+
+
+arr=[1,2,3,4,5,6,7,8,9]
+print(arr)
+print(len(arr))
+print(arr[4])
+
+
+# In[4]:
+
+
+#steps to find sum of array
+#take one
+#declare sum variable 
+
+
+# In[ ]:
+
+
+#linked list
+#they are single double and circular
+#they perform all operations sequentially
+#they are collection of nodes connected by the links in any order 
+
+#operations 
+#createnode()
+#insertnode()
+#deletenode()
+#displaynode() etc
+
+#applications
+#used in graphs
+#used in playing 
+#in recognizinig palindrome
+
+#Nosql is of graphs based objects base and data base
+#optimization in the on bad network
+
+#advantages
+#access items as long as external liml is maintained 
+#delete existing item without shifting
+#size can be expanded
+
+#disaadvantages
+
+
+# single linked list
+
+# In[ ]:
+
+
+#it is a collection of nodes
+#it is the most basic type of list in c
+#it is simple and dynamically allocated
+#each node has its successor and predecessor
+#here the address of first node is always stored in a refrence node known as front or head and next node is always stored at lastknown as null
+
+
+# INSERT A NEW ELEMETNT AT A GIVEN POINT IN A LINKED LIST
+
+# In[ ]:
+
+
+# Node class
+class Node:
+
+	# Function to initialize the
+	# node object
+	def __init__(self, data):
+
+		# Assign data
+		self.data = data
+	
+		# Initialize next as null
+		self.next = None
+
+# Linked List class
+class LinkedList:
+	
+	# Function to initialize the
+	# Linked List object
+	def __init__(self):
+		self.head = None
+
+
+# to delete node in a list 
+
+# 2 linked list
+
+# In[ ]:
+
+
+#it is connected in both directions 
+#it have 
+# link next prev linked list 
+
+
+# # CWH PY
+
+# In[ ]:
+
+
+
+
